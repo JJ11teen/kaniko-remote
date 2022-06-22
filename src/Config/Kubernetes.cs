5fea580e98aa45a71e7d81e5913729e11838c559
Lucas <lucas.sargent@outlook.com>
2022-06-22 03:51:57 +0000

    fix default ns

--- src/Config/Kubernetes.cs
+++ src/Config/Kubernetes.cs
@@ -4,6 +4,6 @@ namespace KanikoRemote.Config
     {
         public string? Kubeconfig { get; set; }
         public string? Context { get; set; }
-        public string Namespace { get; set; } = "default";
+        public string? Namespace { get; set; }
     }
 }
